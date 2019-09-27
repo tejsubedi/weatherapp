d8b19c4ee7bf1f77b38bbaf68e37f95182ce2e48
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-09-26 21:22:13 -0400

    fetching the city weather data

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,16 +1,31 @@
 const key = "";
 
+//Get weather information
+const getWeather = async (id) => {
+    const base = '';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base+query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+//Get city information
 const getCity = async (city) => {
     const base = '';
     const query = `?apikey=${key}&q=${city}`;
-
-
     const response = await fetch(base + query);
     const data = await response.json();
 
-    console.log(data[0]);
+    return data[0];
 }
 
 getCity('Toronto')
-    .then(data => console.log(data))
+    .then(data => {
+        return getWeather(data.Key)
+    }).then((data) => {
+        console.log(data);
+    }) 
     .catch(err => console.log(err));
+
