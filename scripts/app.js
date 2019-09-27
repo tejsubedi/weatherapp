047596694b7cfad9fe05fc5cc66a857805dcdf7d
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-09-26 22:08:23 -0400

    added functionality to ui

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,26 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+    const cityDets = data.cityDets;
+    const weather = data.weather;
+
+    //updates details template
+    details.innerHTML = `
+                <h5 class="my-3">${cityDets.EnglishName}</h5>
+                <div class="my-3">${weather.WeatherText}</div>
+                <div class="display-4 my-4">
+                    <span>${weather.Temperature.Metric.Value}</span>
+                    <span>&deg;c</span>
+                </div>
+    `
+
+    //remove the d-none class if present
+    if(card.classList.contains('d-none')) {
+        card.classList.remove('d-none');
+    }
+}
 
 const updateCity = async (city) => {
     // console.log(city);
@@ -22,7 +44,7 @@ cityForm.addEventListener('submit', e => {
 
     //update the ui with new city
     updateCity(city)
-        .then(data => console.log(data))
+        .then(data => updateUI(data))
         .catch(err => console.log(err));
 
 });
